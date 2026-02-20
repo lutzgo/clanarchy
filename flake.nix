c0520e8c1dffee8d5fda34a8f27b6b376c2f0625
Lutz Go <lutz@example.com>
2026-02-20 11:51:04 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs.clan-core.url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
+  inputs.nixpkgs.follows = "clan-core/nixpkgs";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "clan-core/nixpkgs";
+
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [
+        inputs.clan-core.flakeModules.default
+      ];
+
+      # https://docs.clan.lol/guides/flake-parts
+      clan = {
+        imports = [ ./clan.nix ];
+      };
+
+      perSystem =
+        { pkgs, inputs', ... }:
+        {
+          devShells.default = pkgs.mkShell { packages = [ inputs'.clan-core.packages.clan-cli ]; };
+
+          # Customize nixpkgs
+          # _module.args.pkgs = import inputs.nixpkgs {
+          #   inherit system;
+          #   config.allowUnfree = true;
+          #   overlays = [ ];
+          # };
+          # clan.pkgs = pkgs;
+        };
+    };
+}
