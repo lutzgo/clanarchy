5a8f82b8076fef7163fc707644a0daa651d07567
Lutz Go <lutz@example.com>
2026-02-22 15:01:07 +0100

    added authorized key for admin user

--- machines/miralda/users/admin.nix
+++ machines/miralda/users/admin.nix
@@ -5,6 +5,9 @@
   users.users.admin = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPo4uZn6hVFTnJ0K7eagj1XL0jVn9t6sSU8RAejhWBy+ clanarchy_admin"
+    ];
   };
 
   home-manager.useGlobalPkgs = true;
